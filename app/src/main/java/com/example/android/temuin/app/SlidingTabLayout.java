2f871566cb14571ef21877226fe6893abac439d4
tommyputranto <tommyputranto1@gmail.com>
2015-06-17 01:04:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/temuin/app/SlidingTabLayout.java
@@ -0,0 +1,10 @@
+package com.example.android.temuin.app;
+
+/**
+ * Created by tommy on 5/31/15.
+ */
+public class SlidingTabLayout {
+    public void setDistributeEvenly(boolean distributeEvenly) {
+        this.distributeEvenly = distributeEvenly;
+    }
+}
